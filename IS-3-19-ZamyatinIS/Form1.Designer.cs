76dce64504d15dd4d674a01d0131525d042b2cbb
zxzmarikzxz@gmail.com <zxzmarikzxz@gmail.com>
2021-12-01 18:24:57 +0500

    fix2

--- IS-3-19-ZamyatinIS/Form1.Designer.cs
+++ IS-3-19-ZamyatinIS/Form1.Designer.cs
@@ -41,6 +41,13 @@ namespace IS_3_19_ZamyatinIS
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // listBox1
@@ -62,7 +69,7 @@ namespace IS_3_19_ZamyatinIS
             // 
             this.button1.Location = new System.Drawing.Point(44, 160);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(126, 96);
+            this.button1.Size = new System.Drawing.Size(126, 50);
             this.button1.TabIndex = 2;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
@@ -141,11 +148,76 @@ namespace IS_3_19_ZamyatinIS
             this.label5.TabIndex = 11;
             this.label5.Text = "Кол-во потоков";
             // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(55, 114);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 20);
+            this.textBox6.TabIndex = 12;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(187, 114);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(100, 20);
+            this.textBox7.TabIndex = 13;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(307, 114);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(100, 20);
+            this.textBox8.TabIndex = 14;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(52, 98);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(30, 13);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "GPU";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(184, 98);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(86, 13);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "Производитель";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(304, 98);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(82, 13);
+            this.label8.TabIndex = 17;
+            this.label8.Text = "Объём памяти";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(44, 239);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(126, 50);
+            this.button2.TabIndex = 18;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
@@ -180,6 +252,13 @@ namespace IS_3_19_ZamyatinIS
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- IS-3-19-ZamyatinIS/Form1.cs
+++ IS-3-19-ZamyatinIS/Form1.cs
@@ -21,43 +21,64 @@ namespace IS_3_19_ZamyatinIS
         {
 
         }
-        class Complect
+        abstract class Complect<T>
         {           
             private int price;
             private int yearofissue;
+            T articul;
+
             public Complect(int pr,int year)
             {
                 price = pr;
                 yearofissue = year;
             }
-            public string Display()
+            public virtual string Display()
             {
                 return($"Цена {price} Год выпуска {yearofissue}");
             }
         }
-        class CP:Complect
+        class CP<T>:Complect<T>
         {
-            private int Chastota;
-            private int Yadra;
-            private int Potoki;
+            private int Chastota { get; set; }
+            private int Yadra { get; set; }
+            private int Potoki { get; set; }
             public CP(int pra,int years,int Chastot,int Yadr,int Potok):base(pra,years)
             {
                 Chastota = Chastot;
                 Yadra = Yadr;
                 Potoki = Potok;
             }
-            public string Display()
+            public override string Display()
             {
-                return ($"Частота {Chastota} Кол-во ядер {Yadra} Кол-во потоков {Potoki}");
+                return base.Display() + ($"Частота {Chastota} Кол-во ядер {Yadra} Кол-во потоков {Potoki}");
+            }
+        }
+        class Videomap<T>:Complect<T>
+        {
+            private int GPU { get; set; }
+            private string Proizvoditel { get; set; }
+            private int Memory { get; set; }
+            public Videomap(int prr, int yea,int GPu, string Proizvoditell, int Memor) : base(prr,yea)
+            {
+                GPU = GPu;
+                Proizvoditel = Proizvoditell;
+                Memory = Memor;
+            }
+            public override string Display()
+            { 
+                return ($"GPU {GPU} Производитель {Proizvoditel} Объём памяти {Memory}");
             }
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            Complect price = new Complect(Convert.ToInt32(textBox1.Text),Convert.ToInt32(textBox2.Text));
-            listBox1.Items.Add(price.Display());
-            CP cp = new CP(Convert.ToInt32(textBox1.Text), Convert.ToInt32(textBox2.Text),Convert.ToInt32(textBox3.Text), Convert.ToInt32(textBox4.Text),Convert.ToInt32(textBox5.Text));
-            listBox1.Items.Add(cp.Display());
+            CP<int> cp = new CP<int>(Convert.ToInt32(textBox1.Text), Convert.ToInt32(textBox2.Text),Convert.ToInt32(textBox3.Text), Convert.ToInt32(textBox4.Text),Convert.ToInt32(textBox5.Text));
+            listBox1.Items.Add(cp.Display());            
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Videomap<int> vp = new Videomap<int>(Convert.ToInt32(textBox1.Text), Convert.ToInt32(textBox2.Text), Convert.ToInt32(textBox6.Text), textBox7.Text, Convert.ToInt32(textBox8.Text));
+            listBox1.Items.Add(vp.Display());
         }
     }
 }
