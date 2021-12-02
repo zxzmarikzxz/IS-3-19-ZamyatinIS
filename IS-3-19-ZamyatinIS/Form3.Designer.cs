f576ee05d560028b7f620c6ba7301a7169a4e32c
Илья <zxzmarikzxz@gmail.com>
2021-12-02 23:44:19 +0500

    fix5

--- IS-3-19-ZamyatinIS/Form3.Designer.cs
+++ IS-3-19-ZamyatinIS/Form3.Designer.cs
@@ -30,22 +30,57 @@ namespace IS_3_19_ZamyatinIS
         private void InitializeComponent()
         {
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3});
             this.dataGridView1.Location = new System.Drawing.Point(44, 57);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(240, 150);
+            this.dataGridView1.Size = new System.Drawing.Size(339, 286);
             this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(423, 131);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(175, 140);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "ID";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "FIO";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "theme_kurs";
+            this.Column3.Name = "Column3";
             // 
             // Form3
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.dataGridView1);
             this.Name = "Form3";
             this.Text = "Form3";
@@ -58,5 +93,9 @@ namespace IS_3_19_ZamyatinIS
         #endregion
 
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.Button button1;
     }
 }
--- IS-3-19-ZamyatinIS/Form3.cs
+++ IS-3-19-ZamyatinIS/Form3.cs
@@ -28,6 +28,9 @@ namespace IS_3_19_ZamyatinIS
                 return conn; //Возврат строки подключения
             }
         }
+        private BindingSource bSource = new BindingSource(); //Унифицированный доступ к источнику данных          
+        private DataTable table = new DataTable();
+        private MySqlDataAdapter adapter = new MySqlDataAdapter(); //Получение данных из источника  
         public Form3()
         {
             InitializeComponent();
@@ -35,6 +38,43 @@ namespace IS_3_19_ZamyatinIS
 
         private void Form3_Load(object sender, EventArgs e)
         {
+
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            {
+                ConnBaza conbaza = new ConnBaza();
+                try
+                {
+                    conbaza.ConnBaz().Open();
+                    string commandStr = "SELECT id AS 'ID', fio AS 'ФИО', theme_kurs AS 'Тема' FROM t_stud";//Запрос к базе данных(взять id,fio и theme_kurs из таблицы t_stud)
+                    MySqlDataAdapter adapter = new MySqlDataAdapter(commandStr, conbaza.ConnBaz());
+                    DataTable dTable = new DataTable();
+                    adapter.Fill(dTable);
+                    dataGridView1.Rows.Clear();
+                    for (int i = 0; i < dTable.Rows.Count; i++)//Добавление строк пока i не станет больше или равно количеству строк таблицы(dTable.Rows.Count количество строк таблицы)
+                    {
+                        dataGridView1.Rows.Add(dTable.Rows[i].ItemArray);
+                    }
+                }
+                catch (Exception ex)//Обработка исключений
+                {
+                    MessageBox.Show($"{ex}");
+                }
+                finally
+                {
+                    MessageBox.Show("Подключение успешно");
+                    conbaza.ConnBaz().Close();
+                }
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+            MessageBox.Show(dataGridView1.Rows[e.RowIndex].Cells[1].Value.ToString());//Вывод ФИО в MessageBox
 
         }
     }
