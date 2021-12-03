493b940183e19f921c5cf549b3dddbcf0cc1995c
zxzmarikzxz <74228499+zxzmarikzxz@users.noreply.github.com>
2021-12-03 13:15:49 +0500

    Add files via upload
    Добавление библиотеки классов

---
+++ Библиотека классов/Library174/Library174/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using Library174;
+
+namespace Library174
+{
+    public class ConnectDB
+    {
+        public MySqlConnection connDB()
+        {           
+            string host = "caseum.ru";
+            string port = "33333";
+            string user = "test_user";
+            string db = "db_test";
+            string password = "test_pass";
+            string connStr = $"server={host};port={port};user={user};database={db};password={password};";
+            MySqlConnection conn = new MySqlConnection(connStr);
+            return conn;
+        }      
+    }
+}
